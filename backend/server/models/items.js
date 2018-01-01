9650f73c001ee7fd1a657bdc741c12c0e76c6055
Yuval <uvluni@gmail.com>
2018-01-01 12:38:01 +0200

    working item api get/ post

--- backend/server/models/items.js
+++ backend/server/models/items.js
@@ -2,47 +2,37 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ItemSchema = new Schema({
-    title: String,
-    type: String,
-    generalDesc: String,
-    guestAccess: String,
-    price: Number,
-    currency: String,
-    amenities: [
-        {
-            type: String
-        }
-    ],
-    imageUrl: {
-        type: String
-    },
-    address: {
-        country: String,
-        city: String,
-        street: String,
-        number: Number,
-        lat: Number,
-        lng: Number
-    },
-    theSpace: {
-        description: String,
-        guests: Number,
-        beds: Number,
-        bedrooms: Number
-    },
-    userId: {
-        type: Schema.Types.ObjectId,
-        ref: 'user'
-    },
-    reviews: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'review'
-        }
-    ],
-    occupancy: {
-        type: String
-    }
+	title: String,
+	type: String,
+	generalDesc: String,
+	currency: String,
+	price: [
+		{
+			amount: Number,
+			total: Number
+		}
+	],
+	imageUrl: String,
+	address: {
+		country: String,
+		city: String,
+		street: String,
+		number: Number,
+		lat: Number,
+		lng: Number
+	},
+	userId: {
+		type: Schema.Types.ObjectId,
+		ref: 'user'
+	},
+	reviews: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: 'review'
+		}
+	],
+	addedDate: String,
+	lastUpdate: String
 });
 
 const item = mongoose.model('item', ItemSchema);
