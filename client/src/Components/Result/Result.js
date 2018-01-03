82a4bdc48cff5b2612af4e5451377d69e3af35df
Yuval <uvluni@gmail.com>
2018-01-03 07:46:37 +0200

    w/ working reviews

--- client/src/Components/Result/Result.js
+++ client/src/Components/Result/Result.js
@@ -5,16 +5,22 @@ import style from './Result.css';
 
 const Result = ({ item }) => {
 
+  let reviews = item.userId.receivedReviews;
+  let sum = reviews.map(el => el.rating).reduce((max, cur) => (max + cur), 0);
+  let average = sum / reviews.length;
+
   return (
     <div className={style.wrapper}>
       <img className={style.image} src={item.imageUrl} alt={item.title} />
       <h4 className={style.title}>{item.title}</h4>
       <div className={style.contentWrapper}>
-        {/* <h4 className={style.seller}>Seller: <span>{item.seller.user}</span></h4> */}
-        <div className={style.ratingWrapper}>
-          {/* <ReactStars value={item.seller.rating} color2={'#7ABA71'} edit={false} char={'🍁'} size={'20px'} /> */}
-          {/* ({item.seller.reviews} reviews) */}
-        </div>
+        <h4 className={style.seller}>
+          <span>@{item.userId.email.split('@')[0]}</span>
+          <div className={style.ratingWrapper}>
+            <ReactStars className={style.stars} value={average} color2={'#7ABA71'} edit={false} char={'🍁'} size={20} />
+            ({reviews.length} reviews)
+          </div>
+        </h4>
         <Prices prices={item.price} />
         <p>{item.generalDesc}</p>
       </div >
